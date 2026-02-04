32e253c4aa9f5890bf4ead0c1437d3a9525ad57e
patrishr19 <kepp3.yt@gmail.com>
2026-02-04 19:34:18 +0100

    Removed unrelated comments

--- src/main.c
+++ src/main.c
@@ -11,28 +11,6 @@ int main() {
         }
         printf("\n");
     }
-    // FILE *fpointer;
-    // // fpointer = fopen("log.txt", "w");
-
-
-    // // fprintf(fpointer, "hello");
-    
-    // // fpointer = fopen("log.txt", "a");
-    // // fprintf(fpointer, "hello2\n");
-    // fpointer = fopen("log.txt", "r");
-    // char string[100];
-
-    // while(fgets(string, 100, fpointer)) {
-    //     printf("%s", string);
-    // }
    
-    
-
-    // fclose(fpointer);
-
-
-    // uint8_t a = 255;
-
-    // printf("%u\n", a);
     return 0;
 }
