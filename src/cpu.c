f40516d0a1b259b733ab7f8a41ccae05a794046f
patrishr19 <kepp3.yt@gmail.com>
2026-02-11 18:00:42 +0100

    opcodes: 0x0C, 0x0D, 0x08, 0x88, 0x89, 0x0E, 0xDD + 0xCC fix
    opcode changes

--- src/cpu.c
+++ src/cpu.c
@@ -23,6 +23,8 @@ void CPUInit(CPU *cpu) { // this skips the nintendo logo and boot up sequence
 int CPUStep(CPU *cpu, Bus *bus) {
     uint8_t opcode = BusRead(bus, cpu->pc);
     printf("Opcode at 0x%04X: 0x%02X\n", cpu->pc, opcode);
+
+    
     cpu->pc++;
     switch(opcode) { // opcodes
         case 0x00: {
@@ -90,7 +92,7 @@ int CPUStep(CPU *cpu, Bus *bus) {
                 cpu->sp -= 2; // space for 2 bytes
 
                 BusWrite(bus, cpu->sp, cpu->pc & 0xFF);
-                BusWrite(bus, cpu->sp + 1, (cpu->pc << 8) & 0xFF);
+                BusWrite(bus, cpu->sp + 1, (cpu->pc >> 8) & 0xFF);
                 cpu->pc = address;
                 return 24;
             }
@@ -135,8 +137,122 @@ int CPUStep(CPU *cpu, Bus *bus) {
 
             return 4;
         }
-        case 0x0C: {
-            // to do
+        case 0x0C: { // INC C     1  4    Z 0 H -
+            //
+            uint8_t result = cpu->c + 1;
+            cpu->c = result & 0xFF;
+
+            if (cpu->c == 0) {
+                cpu->f |= 0x80;
+            } else {
+                cpu->f &= ~0x80;
+            }
+
+            cpu->f &= ~0x40;
+
+            if ((cpu->c ^ result) &  0x10) {
+                cpu->f |= 0x20;
+            } else {
+                cpu->f &= ~0x20;
+            }
+
+            return 4;
+        }
+        case 0x0D: { // DEC C    1  4    Z 1 H -
+            uint8_t result = cpu->c - 1;
+            cpu->c = result & 0xFF;
+
+            if (cpu->c == 0) {
+                cpu->f |= 0x80;
+            } else {
+                cpu->f &= ~0x80;
+            }
+
+            cpu->f |= 0x40;
+
+            if ((cpu->c ^ result) & 0x10) {
+                cpu->f |= 0x20;
+            } else {
+                cpu->f &= ~0x20;
+            }
+
+            return 4;
+        }
+        case 0x08: { // LD [a16], SP    3  20    - - - -
+            uint16_t address = BusRead(bus, cpu->pc);
+            address |= BusRead(bus, cpu->pc + 1) << 8;
+
+            cpu->pc += 2;
+
+            BusWrite(bus, address, cpu->sp & 0xFF);
+            BusWrite(bus, address + 1, (cpu->sp >> 8) & 0xFF);
+            
+            return 20;
+        }
+        case 0x88: { // ADC A, B    1  4     Z 0 H C
+            uint16_t result = cpu->a + cpu->b + (cpu->f & 0x10 ? 1 : 0);
+
+            cpu->a = result & 0xFF;
+
+            if (cpu->a == 0) {
+                cpu->f |= 0x80;
+            } else {
+                cpu->f &= ~0x80;
+            }
+
+            cpu->f &= ~0x40;
+
+            if ((cpu->a ^ cpu->b ^ result) & 0x10) {
+                cpu->f |= 0x20;
+            } else {
+                cpu->f &= ~0x20;
+            }
+
+            if (result > 0xFF) {
+                cpu->f |= 0x10;
+            } else {
+                cpu->f &= ~0x10;
+            }
+
+            return 4;
+        }
+        case 0x89: { // ADC A, C    1  4    Z 0 H C
+            uint16_t result = cpu->a + cpu->c + (cpu->f & 0x10 ? 1 : 0);
+
+            cpu->a = result & 0xFF;
+
+            if (cpu->a == 0) {
+                cpu->f |= 0x80;
+            } else {
+                cpu->f &= ~0x80;
+            }
+
+            cpu->f &= ~0x40;
+
+            if ((cpu->a ^ cpu->c ^ result) & 0x10) {
+                cpu->f |= 0x20;
+            } else {
+                cpu->f &= ~0x20;
+            }
+
+            if (result > 0xFF) {
+                cpu->f |= 0x10;
+            } else {
+                cpu->f &= ~0x10;
+            }
+            
+            return 4;
+        }
+        case 0x0E: { // LD C, n8     2  8   - - - -
+            uint8_t value = BusRead(bus, cpu->pc);
+            cpu->pc++;
+            
+            cpu->c = value;
+
+            return 8;
+        }
+        case 0xDD: { // COME BACK LATER!!!!!!
+            return 4;
         }
         default:
             printf("Crash: opcode 0x%02X at pc 0x%04X\n", opcode, cpu->pc - 1);
