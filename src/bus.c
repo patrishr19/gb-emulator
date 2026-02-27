d86dfac52a43b2bb841ea362145f340e6ead3621
Patrik Borza <kepp3.yt@gmail.com>
2026-02-27 12:54:17 +0100

    slight change of the bus read and write structure

--- src/bus.c
+++ src/bus.c
@@ -4,23 +4,18 @@
 #include <iogm.h>
 
 uint8_t BusRead(Bus *bus, uint16_t address) {
-    // ROM 0x0000 - 0x7FFF
-    // if (address >= 0x0000 && address <= 0x7FFF) {
-    //     return bus->memory[address];
-    // }
+    if (address >= 0xE000 && address <= 0xFDFF) {
+        address -= 0x2000;
+    }
 
-    // VRAM
-    /*
-        if (address >= 0x8000 && address <= 0x9FFF) {
-            return 0;
-        }
-    */
+    if (address >= 0xFF00 && address < 0xFFFF) {
+        return IORead(&bus->io, address - 0xFF00);
+    }
+
     if (address == 0xFFFF) {
         return IORead(&bus->io, 0xFF);
     }
-    if (address >= 0xFF00) {
-        return IORead(&bus->io, address - 0xFF00);
-    }
+
     return bus->memory[address]; //for now
     
 }
@@ -30,24 +25,28 @@ void BusWrite(Bus *bus, uint16_t address, uint8_t value) {
         fflush(stdout);
     }
 
+    if (address < 0x8000) {
+        //TODO MBC
+        return;
+    }
+
+    if (address >= 0xE000 && address <= 0xFDFF) {
+        address -= 0x2000;
+    }
+
+    // io registers
+    if (address >= 0xFF00 && address < 0xFFFF) {
+        IOWrite(&bus->io, address - 0xFF00, value);
+        return;
+    }
+
     // ei register
     if (address == 0xFFFF) {
         IOWrite(&bus->io, 0xFF, value);
         return;
     }
-
-    // io registers
-    if (address >= 0xFF00) {
-        IOWrite(&bus->io, address - 0xFF00, value);
-        return;
-    }
-    // TODO rom protection
-    if (address < 0x8000) {
-        return; 
-    }
-
-    bus->memory[address] = value;
     
+    bus->memory[address] = value;
 }
 
 uint16_t BusRead16(Bus *bus, uint16_t address) {
