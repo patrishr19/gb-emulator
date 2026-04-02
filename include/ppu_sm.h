602de6d50936451dfac4e87e12f0ba1a972e1562
Patrik Borza <kepp3.yt@gmail.com>
2026-04-02 10:22:45 +0200

    ppu modes

---
+++ include/ppu_sm.h
@@ -0,0 +1,12 @@
+#ifndef PPU_SM_H
+#define PPU_SM_H
+
+#include <setup.h>
+
+void ppu_mode_oam();
+void ppu_mode_xfer();
+void ppu_mode_vblank(Bus *bus);
+void ppu_mode_hblank(Bus *bus);
+
+void increment_ly(Bus *bus);
+#endif
